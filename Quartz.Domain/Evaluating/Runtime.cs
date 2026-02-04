fc0942bbd3c0c6c8257e3b9f7b03a0840f99bcae
Arman <eccs0103@gmail.com>
2026-02-05 03:43:45 +0400

    0.5.0

--- Quartz.Domain/Evaluating/Runtime.cs
+++ Quartz.Domain/Evaluating/Runtime.cs
@@ -14,51 +14,20 @@ public class Runtime
 		{
 			module.DeclareClass("Any", null, static (type) =>
 			{
-				type.DeclareOperation("is_equal", ["Any", "Any"], "Boolean", static (object a, object b) =>
+				type.DeclareOperation("is_equal", ["Any", "Any"], "Boolean", static (object @this, object other) =>
 				{
-					if (a == null) return b == null;
-					return a.Equals(b);
+					if (@this == null) return other == null;
+					return @this.Equals(other);
 				});
-				type.DeclareOperation("=", ["Any", "Any"], "Boolean", static (object a, object b) =>
+				type.DeclareOperation("=", ["Any", "Any"], "Boolean", static (object @this, object other) =>
 				{
-					if (a == null) return b == null;
-					return a.Equals(b);
+					if (@this == null) return other == null;
+					return @this.Equals(other);
 				});
-				type.DeclareOperation("!=", ["Any", "Any"], "Boolean", static (object a, object b) =>
+				type.DeclareOperation("!=", ["Any", "Any"], "Boolean", static (object @this, object other) =>
 				{
-					if (a == null) return b != null;
-					return !a.Equals(b);
-				});
-			});
-			module.DeclareClass("Type", "Any", static (type) =>
-			{
-			});
-			module.DeclareClass("Function", "Any", static (type) =>
-			{
-			});
-			module.DeclareClass("Null", "Any", static (type) =>
-			{
-			});
-			module.DeclareClass("Boolean", "Any", static (type) =>
-			{
-				type.DeclareOperation("!", ["Boolean"], "Boolean", static (bool @this) =>
-				{
-					return !@this;
-				});
-				type.DeclareOperation("&", ["Boolean", "Boolean"], "Boolean", static (bool @this, bool other) =>
-				{
-					return @this && other;
-				});
-				type.DeclareOperation("|", ["Boolean", "Boolean"], "Boolean", static (bool @this, bool other) =>
-				{
-					return @this || other;
-				});
-			});
-			module.DeclareClass("String", "Any", static (type) =>
-			{
-				type.DeclareOperation("+", ["String", "String"], "String", static (string @this, string other) =>
-				{
-					return @this + other;
+					if (@this == null) return other != null;
+					return !@this.Equals(other);
 				});
 			});
 			module.DeclareClass("Number", "Any", static (type) =>
@@ -104,6 +73,37 @@ public class Runtime
 					return @this >= other;
 				});
 			});
+			module.DeclareClass("Boolean", "Any", static (type) =>
+			{
+				type.DeclareOperation("!", ["Boolean"], "Boolean", static (bool @this) =>
+				{
+					return !@this;
+				});
+				type.DeclareOperation("&", ["Boolean", "Boolean"], "Boolean", static (bool @this, bool other) =>
+				{
+					return @this && other;
+				});
+				type.DeclareOperation("|", ["Boolean", "Boolean"], "Boolean", static (bool @this, bool other) =>
+				{
+					return @this || other;
+				});
+			});
+			module.DeclareClass("String", "Any", static (type) =>
+			{
+				type.DeclareOperation("+", ["String", "String"], "String", static (string @this, string other) =>
+				{
+					return @this + other;
+				});
+			});
+			module.DeclareClass("Null", "Any", static (type) =>
+			{
+			});
+			module.DeclareClass("Function", "Any", static (type) =>
+			{
+			});
+			module.DeclareClass("Type", "Any", static (type) =>
+			{
+			});
 			module.DeclareClass(RuntimeBuilder.NameWorkspace, "Any", static (type) =>
 			{
 				type.DeclareConstant("pi", "Number", PI);
@@ -127,9 +127,9 @@ public class Runtime
 				{
 					Console.WriteLine(value);
 				});
-				type.DeclareOperation("write", ["Null"], "Null", static () =>
+				type.DeclareOperation("write", ["Null"], "Null", static (object? value) =>
 				{
-					Console.WriteLine("null");
+					Console.WriteLine(value?.ToString() ?? "null");
 				});
 			});
 		});
